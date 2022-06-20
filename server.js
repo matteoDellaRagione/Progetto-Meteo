d8daf39ce694e221d57715219ed42ac5fb69289b
matteoDellaRagione <dellaragionematteo02@gmail.com>
2022-06-20 20:55:08 +0200

    Mongodb

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-
+const { MongoClient } = require('mongodb')
 const express = require('express');
 const path = require('path');
+require('dotenv').config()
+const mongoClient = new MongoClient(process.env.mongoDB_api)
+const bodyparser = require('body-parser')
 
  //Configure dotenv package
  require("dotenv").config();
@@ -23,5 +26,31 @@ app.get('/index', function (req, res) {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
+app.post('/auth', bodyparser.urlencoded(), (req,res) => {
+    console.log(req.body);
+    checkAuth(req.body.email, req.body.password).then(data => {
+        if(data.length){
+            res.end('autenticato')
+        }else
+            res.end('non autenticato')
+    })
+})
+
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
+
+checkAuth = async (email, password) => {
+    try{
+        await mongoClient.connect()
+        const database = mongoClient.db(process.env.database)
+        const users = database.collection(process.env.collection)
+        let result = await users.find(
+            { email: email, password: password}
+        ).toArray()
+        console.log(result);
+        return Promise.resolve(result)
+    }catch (error) {
+        return Promise.reject([])
+    }
+}
+
